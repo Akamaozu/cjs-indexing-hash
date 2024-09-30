05c1fb424dbe04dc996fddc3d7351ab21faecdd9
uzo olisemeka <uzo@designbymobius.ca>
2024-09-30 14:50:44 -0400

    data: added indexing_hash_properties, types

---
+++ data/indexing-hash-properties.js
@@ -0,0 +1,69 @@
+const INDEXING_HASH_PROPERTIES = {
+  hooks: {
+    key: 'hooks',
+    type: 'object',
+    required: true,
+  },
+  keys: {
+    key: 'keys',
+    type: 'function',
+    required: true,
+  },
+  get: {
+    key: 'get',
+    type: 'function',
+    required: true,
+  },
+  add: {
+    key: 'add',
+    type: 'function',
+    required: true,
+  },
+  del: {
+    key: 'del',
+    type: 'function',
+    required: true,
+  },
+  update: {
+    key: 'update',
+    type: 'function',
+    required: true,
+  },
+  indexes: {
+    key: 'indexes',
+    type: 'function',
+    required: true,
+  },
+  index_exists: {
+    key: 'index_exists',
+    type: 'function',
+    required: true,
+  },
+  index_get: {
+    key: 'index_get',
+    type: 'function',
+    required: true,
+  },
+  add_index: {
+    key: 'add_index',
+    type: 'function',
+    required: true,
+  },
+  del_index: {
+    key: 'del_index',
+    type: 'function',
+    required: true,
+  },
+  reindex: {
+    key: 'reindex',
+    type: 'function',
+    required: true,
+  },
+  intersect_indexes: {
+    key: 'intersect_indexes',
+    type: 'function',
+    required: true,
+  },
+}
+
+module.exports = INDEXING_HASH_PROPERTIES
