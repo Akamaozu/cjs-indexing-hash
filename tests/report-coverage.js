aad8b26b00f6181ff3006d9667c7fa2da7e3c39a
uzo olisemeka <uzo@designbymobius.ca>
2024-10-01 20:38:57 -0400

    minor code cleanup

--- tests/report-coverage.js
+++ tests/report-coverage.js
@@ -5,5 +5,5 @@ const child_process = require('node:child_process')
 dotenv.config({ path: path.join( __dirname, '../.env' ) })
 
 child_process.exec( 'npm run test:coverage && nyc report --reporter=text-lcov | coveralls', ( error, stdout, stderr ) => {
-  console.log( error ? stderr : stdout )
+  console.log( error ? stderr : stdout  )
 })
